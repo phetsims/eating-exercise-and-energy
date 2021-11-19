874079efa76a4771d0b4a23660df62979f7bfb48
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:25 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

--- js/eatingExerciseAndEnergyStrings.ts
+++ js/eatingExerciseAndEnergyStrings.ts
@@ -3,11 +3,17 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import eatingExerciseAndEnergy from './eatingExerciseAndEnergy.js';
 
-const eatingExerciseAndEnergyStrings = getStringModule( 'EATING_EXERCISE_AND_ENERGY' );
+type StringsType = {
+  'eating-exercise-and-energy': {
+    'title': string
+  }
+};
+
+const eatingExerciseAndEnergyStrings = getStringModule( 'EATING_EXERCISE_AND_ENERGY' ) as StringsType;
 
 eatingExerciseAndEnergy.register( 'eatingExerciseAndEnergyStrings', eatingExerciseAndEnergyStrings );
 
