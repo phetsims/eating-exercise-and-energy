c89d003e59dbaa0a8b8bd32346838fbd1bb98626
Sam Reid <reids@colorado.edu>
2022-09-02 12:02:28 -0600

    Change Sim constructor's title parameter from string to TReadOnlyProperty<string>, and update main call sites, see https://github.com/phetsims/joist/issues/844

--- js/eating-exercise-and-energy-main.js
+++ js/eating-exercise-and-energy-main.js
@@ -12,7 +12,7 @@ import Tandem from '../../tandem/js/Tandem.js';
 import EatingExerciseAndEnergyScreen from './eating-exercise-and-energy/EatingExerciseAndEnergyScreen.js';
 import eatingExerciseAndEnergyStrings from './eatingExerciseAndEnergyStrings.js';
 
-const eatingExerciseAndEnergyTitleString = eatingExerciseAndEnergyStrings[ 'eating-exercise-and-energy' ].title;
+const eatingExerciseAndEnergyTitleStringProperty = eatingExerciseAndEnergyStrings[ 'eating-exercise-and-energy' ].titleStringProperty;
 
 const simOptions = {
 
@@ -31,7 +31,7 @@ const simOptions = {
 // launch the sim - beware that scenery Image nodes created outside of simLauncher.launch() will have zero bounds
 // until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
 simLauncher.launch( () => {
-  const sim = new Sim( eatingExerciseAndEnergyTitleString, [
+  const sim = new Sim( eatingExerciseAndEnergyTitleStringProperty, [
     new EatingExerciseAndEnergyScreen( Tandem.ROOT.createTandem( 'eatingExerciseAndEnergyScreen' ) )
   ], simOptions );
   sim.start();
