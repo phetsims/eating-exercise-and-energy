16fe525ea77a251c294ff6f0d5febae658d3a283
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-20 12:16:14 -0600

    remove as many LinkableElement usages as possible, using PhetioProperty where we have to, https://github.com/phetsims/tandem/issues/299
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/EatingExerciseAndEnergyStrings.ts
+++ js/EatingExerciseAndEnergyStrings.ts
@@ -5,13 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import LinkableProperty from '../../axon/js/LinkableProperty.js';
+import type LocalizedStringProperty from '../../chipper/js/LocalizedStringProperty.js';
 import eatingExerciseAndEnergy from './eatingExerciseAndEnergy.js';
 
 type StringsType = {
   'eating-exercise-and-energy': {
     'title': string;
-    'titleStringProperty': LinkableProperty<string>;
+    'titleStringProperty': LocalizedStringProperty;
   }
 };
 
