487d8f1a4a807238f4356fe572f198ce588a9748
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:14 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for eating-exercise-and-energy.
+ * ESLint configuration for eating-exercise-and-energy.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
